d1d822baf31e223a39e19ee953e5805420ba921f
Tato <italoluna23@gmail.com>
2025-07-30 20:20:40 -0500

    add code changes to enhance functionality and improve performance

--- src/why-choose-us/pages/why-choose-us.component.vue
+++ src/why-choose-us/pages/why-choose-us.component.vue
@@ -123,6 +123,80 @@
 
     </div>
   </section>
+
+
+  <!-- ¿Por qué invertir en Perú? -->
+  <section class="relative overflow-hidden xl:h-[900px] lg:h-[950px] md:h-[900px] h-[1200px]">
+
+    <!-- Fondo -->
+    <img src="../../assets/backgrounds/lima-plaza.jpg" alt="Lima Plaza" class="absolute w-full h-full object-cover" />
+
+    <!-- Contenido principal -->
+    <div class="relative w-full max-w-screen-xl mx-auto px-4 xl:px-0 pt-20 pb-10">
+
+      <!-- Titulo -->
+      <div class="flex flex-col lg:flex-row lg:items-start justify-between lg:gap-25 gap-8">
+        <!-- vineta -->
+        <SectionTitle title="Creando puentes" textColor="text-white" class="text-nowrap text-shadow-lg/25" />
+
+        <!-- Titulo -->
+        <div class="flex flex-col gap-2">
+          <div class="space-y-0 md:space-y-2">
+            <p class="text-4xl md:text-5xl xl:text-7xl font-mont-regular text-nowrap text-white text-shadow-lg/25">
+              ¿Por qué <span class="text-secondary font-mont-heavy">invertir</span>
+            </p>
+            <p class="text-4xl md:text-5xl xl:text-7xl font-mont-heavy text-nowrap text-white text-shadow-lg/25">
+              en <span class="text-secondary font-mont-heavy">Perú</span>?
+            </p>
+          </div>
+          <div class="flex">
+            <p class="font-mont-regular lg:text-xl text-white text-shadow-lg/25">
+              Elegir New Point es apostar por una visión estratégica y confiable para invertir en Perú, uno de los
+              destinos
+              más competitivos de América Latina para la inversión y el comercio internacional.
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <!-- Tarjetas -->
+      <div class="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-5 gap-6 md:mt-15 lg:mt-30 mt-10">
+        <!-- Card -->
+        <div class="bg-tertiary xl:p-6 lg:p-6 p-5 w-full flex flex-col items-center text-center md:gap-5 gap-3">
+          <p class="text-white font-mont-heavy md:text-xl text-base">Apertura comercial global</p>
+          <p class="text-white md:text-base text-sm">Acceso a mercados internacionales y tratados de libre comercio.</p>
+        </div>
+
+        <div class="bg-tertiary xl:p-6 lg:p-6 p-5 w-full flex flex-col items-center text-center gap-5">
+          <p class="text-white font-mont-heavy md:text-xl text-base px-11">Ubicación estratégica</p>
+          <p class="text-white md:text-base text-sm">Conectividad logística privilegiada con América y el mundo.</p>
+        </div>
+
+        <div class="bg-tertiary xl:p-6 lg:p-6 p-5 w-full flex flex-col items-center text-center gap-5">
+          <p class="text-white font-mont-heavy md:text-xl text-base">Fortaleza macroeconómica</p>
+          <p class="text-white md:text-base text-sm">Entorno económico estable, crecimiento sostenido e inversión.</p>
+        </div>
+
+        <div class="bg-tertiary xl:p-6 lg:p-6 p-5 w-full flex flex-col items-center text-center gap-5">
+          <p class="text-white font-mont-heavy md:text-xl text-base">Potencial exportador global</p>
+          <p class="text-white md:text-base text-sm">Oferta competitiva y diversificada con alcance internacional.</p>
+        </div>
+
+        <div class="bg-tertiary xl:p-6 lg:p-6 p-5 w-full flex flex-col items-center text-center gap-5">
+          <p class="text-white font-mont-heavy md:text-xl text-base">Diversificación y resiliencia</p>
+          <p class="text-white md:text-base text-sm">Economía adaptable con sectores sólidos y políticas proactivas.</p>
+        </div>
+      </div>
+
+      <!-- Etiquetas flotantes -->
+      <!-- <span
+        class="absolute left-0 bottom-24 rotate-[-15deg] bg-white text-black px-6 py-1 rounded-full shadow-lg font-mont-bold text-lg border border-black/20">Inversión</span>
+      <span
+        class="absolute right-0 top-32 rotate-[15deg] bg-yellow-200 text-black px-6 py-1 rounded-full shadow-lg font-mont-bold text-lg border border-black/20">Comercio</span> -->
+    </div>
+  </section>
+
 </template>
 
 <script>
